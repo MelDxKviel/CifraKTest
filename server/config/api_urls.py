168d1b405f752216e44e75b17ce1d0c0accf50bf
meldxkviel <mega1hack52@gmail.com>
2022-09-13 23:40:53 +0700

    Add API documentation

--- server/config/api_urls.py
+++ server/config/api_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
 from apps.news.routers import news_router
+from rest_framework.schemas import get_schema_view
 
 
 urlpatterns = [
-    path('', include(news_router.urls))
+    path('', include(news_router.urls)),
+    path('schema/', get_schema_view())
 ]
